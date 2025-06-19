71c72085a2f69309266a85eea996529b32a18ffa
Aaron Danen <aaron.danen@gmail.com>
2025-06-19 11:53:44 -0700

    added a few basic instructions

--- include/chip8.h
+++ include/chip8.h
@@ -1,3 +1,6 @@
+#ifndef CHIP8_H
+#define CHIP8_H
+
 #include <stdint.h>
 #define CHIP8_SCREEN_WIDTH 64
 #define CHIP8_SCREEN_HEIGHT 32
@@ -6,7 +9,7 @@
 #define CHIP8_STACK_DEPTH 16
 #define CHIP8_CLOCK_LENGTH 1.0/700.0
 
-extern uint8_t chip8_screen[CHIP8_SCREEN_HEIGHT/8][CHIP8_SCREEN_WIDTH/8];
+extern uint8_t chip8_screen[CHIP8_SCREEN_HEIGHT][CHIP8_SCREEN_WIDTH];
 extern uint8_t chip8_delay;
 extern uint8_t chip8_sound;
 
@@ -14,4 +17,4 @@ void CHIP8_initialize();
 void CHIP8_load(char* rom_path);
 void CHIP8_cycle();
 
-
+#endif /* CHIP8_H */
--- src/chip8.c
+++ src/chip8.c
@@ -1,10 +1,11 @@
 #include <stdint.h>
+#include <stdio.h>
 #include <string.h>
 #include <chip8.h>
 #include <stdlib.h>
 
-uint8_t chip8_ram[chip8_ram_SIZE];
-uint8_t chip8_screen[CHIP8_SCREEN_HEIGHT/8][CHIP8_SCREEN_WIDTH/8];
+uint8_t chip8_ram[CHIP8_RAM_SIZE];
+uint8_t chip8_screen[CHIP8_SCREEN_HEIGHT][CHIP8_SCREEN_WIDTH];
 uint8_t chip8_delay = 0;
 uint8_t chip8_sound = 0;
 uint8_t chip8_v[CHIP8_NUM_REGISTERS];
@@ -33,6 +34,64 @@ uint8_t FONT_DATA[5*16] = {
   0xF0, 0x80, 0xF0, 0x80, 0x80  // F
 };
 
+// INSTRUCTIONS //
+
+// clear screen. turn all pixels in display buffer to 0
+void CHIP8_00E0() {
+  memset(chip8_screen, 0, sizeof(chip8_screen));
+}
+
+void CHIP8_00EE() {
+}
+
+// jump to NNN
+void CHIP8_1NNN(uint16_t NNN) {
+  chip8_pc = NNN;
+}
+
+// set VX to NN
+void CHIP8_6XNN(uint8_t X, uint8_t NN) {
+  chip8_v[X] = NN;
+}
+
+// add NN to VX
+void CHIP8_7XNN(uint8_t X, uint8_t NN) {
+  chip8_v[X] += NN;
+}
+
+// set chip8_index to NNN
+void CHIP8_ANNN(uint16_t NNN) {
+  chip8_index = NNN;
+}
+
+// display!
+void CHIP8_DXYN(uint8_t X, uint8_t Y, uint8_t N) {
+  uint8_t x = chip8_v[X] % CHIP8_SCREEN_WIDTH;
+  uint8_t y = chip8_v[Y] % CHIP8_SCREEN_HEIGHT;
+  chip8_v[0xf] = 0;
+
+  // for N rows
+  for (uint8_t i = 0; i < N; i++) {
+    uint8_t row = chip8_ram[chip8_index+i];
+    // for each bit in the row
+    for (uint8_t offset = 7; offset >= 0; offset--) {
+      uint8_t spritebit = (row >> offset) & 0x1;
+      uint8_t pixel = chip8_screen[y + i][x + (7-offset)];
+      if (spritebit && pixel) {
+        chip8_screen[y + i][x + (7-offset)] = 0;
+        chip8_v[0xf] = 1;
+      } else if (spritebit && !pixel) {
+        chip8_screen[y + i][x + (7-offset)] = 1;
+      }
+    }
+   
+    if (y + i == CHIP8_SCREEN_HEIGHT) {
+      return;
+    }
+  }
+}
+
+
 
 // set up the CHIP8
 // * loads font into RAM
@@ -43,10 +102,7 @@ void CHIP8_initialize() {
 
 
 void CHIP8_load(char* rom_path) {
-}
-
-
-void CHIP8_cycle() {
+  return;
 }
 
 
@@ -89,5 +145,33 @@ void CHIP8_cycle() {
   uint8_t N = (uint8_t)(instruction & 0x000f);
   uint8_t NN = (uint8_t)(instruction & 0x00ff);
   uint16_t NNN = instruction & 0x0fff;
+  switch (opcode) {
+    case 0x0:
+      if (NN == 0xE0) {
+        CHIP8_00E0();
+      } else {
+        CHIP8_00EE();
+      }
+      break;
+    case 0x1:
+      CHIP8_1NNN(NNN);
+      break;
+    case 0x6:
+      CHIP8_6XNN(X, NN);
+      break;
+    case 0x7:
+      CHIP8_7XNN(X, NN);
+      break;
+    case 0xA:
+      CHIP8_ANNN(NNN);
+      break;
+    case 0xD:
+      CHIP8_DXYN(X, Y, N);
+      break;
+  }
 }
+
+
+
+
 
